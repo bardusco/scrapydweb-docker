fd403bd5fc21a40f0fbddadc524dd946e0bcab8f
Danilo Bardusco <danilo@bardusco.com>
2023-07-04 16:23:23 -0300

    Disable LogParser in quick setup configuration

--- scrapydweb_settings_v10.py
+++ scrapydweb_settings_v10.py
@@ -70,7 +70,7 @@ LOCAL_SCRAPYD_LOGS_DIR = ''
 # Note that you can run the LogParser service separately via command 'logparser' as you like.
 # Run 'logparser -h' to find out the config file of LogParser for more advanced settings.
 # Visit https://github.com/my8100/logparser for more info.
-ENABLE_LOGPARSER = True
+ENABLE_LOGPARSER = False
 ############################## QUICK SETUP end ################################
 ############################## 快速设置 结束 ###################################
 
